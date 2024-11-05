7d6a5c07ece9107e4786d834ab05ac0b8f6c3acd
Rishabh Singh <124164455+DMGXI@users.noreply.github.com>
2024-11-05 13:01:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+// Automatically change slide every 5 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
